46356194822b19a3e4a0e7aa4e1fbd64c01a4488
Vadim <vadimryabev@yandex.ru>
2023-05-10 19:22:17 +0300

    v2

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-function smoothScroll(target, duration) {
-  var targetSection = document.querySelector(target);
-  var targetPosition = targetSection.offsetTop;
-  var startPosition = window.pageYOffset;
-  var distance = targetPosition - startPosition;
-  var startTime = null;
+// function smoothScroll(target, duration) {
+//   var targetSection = document.querySelector(target);
+//   var targetPosition = targetSection.offsetTop;
+//   var startPosition = window.pageYOffset;
+//   var distance = targetPosition - startPosition;
+//   var startTime = null;
 
-  function animation(currentTime) {
-    if (startTime === null) startTime = currentTime;
-    var timeElapsed = currentTime - startTime;
-    var run = ease(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, run);
-    if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
+//   function animation(currentTime) {
+//     if (startTime === null) startTime = currentTime;
+//     var timeElapsed = currentTime - startTime;
+//     var run = ease(timeElapsed, startPosition, distance, duration);
+//     window.scrollTo(0, run);
+//     if (timeElapsed < duration) requestAnimationFrame(animation);
+//   }
 
-  function ease(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
+//   function ease(t, b, c, d) {
+//     t /= d / 2;
+//     if (t < 1) return c / 2 * t * t + b;
+//     t--;
+//     return -c / 2 * (t * (t - 2) - 1) + b;
+//   }
 
-  requestAnimationFrame(animation);
-}
+//   requestAnimationFrame(animation);
+// }
 
-let navbarLinks = document.querySelectorAll('.link');
-let logo = document.querySelector('.logo')
-let up = document.querySelector('#up')
+// let navbarLinks = document.querySelectorAll('.link');
+// let logo = document.querySelector('.logo')
+// let up = document.querySelector('#up')
 
-logo.addEventListener('click', scroll)
-navbarLinks.forEach(link => link.addEventListener('click', scroll));
-up.addEventListener('click', scroll)
+// logo.addEventListener('click', scroll)
+// navbarLinks.forEach(link => link.addEventListener('click', scroll));
+// up.addEventListener('click', scroll)
 
-function scroll(e) {
-  //e.preventDefault();
-    var target = this.getAttribute('href');
-    var duration = 1000;
-    smoothScroll(target, duration);
-}
+// function scroll(e) {
+//   //e.preventDefault();
+//     var target = this.getAttribute('href');
+//     var duration = 1000;
+//     smoothScroll(target, duration);
+// }
 
 
-function burgerMenu() {
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.nav-links')
-  const body = document.querySelector('body')
-  burger.addEventListener('click', () => {
-    if (!menu.classList.contains('active')) {
-      menu.classList.add('active')
-      burger.classList.add('active-burger')
-      body.classList.add('locked')
-    } else {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    }
-  })
-  menu.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', () => {
-          menu.classList.remove('active')
-          burger.classList.remove('active-burger')
-          body.classList.remove('locked')
-        })
-  })
-}
-burgerMenu()
+// function burgerMenu() {
+//   const burger = document.querySelector('.burger')
+//   const menu = document.querySelector('.nav-links')
+//   const body = document.querySelector('body')
+//   burger.addEventListener('click', () => {
+//     if (!menu.classList.contains('active')) {
+//       menu.classList.add('active')
+//       burger.classList.add('active-burger')
+//       body.classList.add('locked')
+//     } else {
+//       menu.classList.remove('active')
+//       burger.classList.remove('active-burger')
+//       body.classList.remove('locked')
+//     }
+//   })
+//   menu.querySelectorAll('a').forEach(link => {
+//         link.addEventListener('click', () => {
+//           menu.classList.remove('active')
+//           burger.classList.remove('active-burger')
+//           body.classList.remove('locked')
+//         })
+//   })
+// }
+// burgerMenu()
 
 
 // function smoothScroll(target, duration) {
@@ -105,3 +105,12 @@ burgerMenu()
 //   });
 // });
 // Этот код получает все ссылки в навбаре и добавляет обработчик событий клика. Когда пользователь кликает на ссылку, функция `smoothScroll` вызывается с аргументами `target` и `duration`, которые получаются из атрибутов `href` и `data-duration` соответственно. Функция `preventDefault` предотвращает стандартное поведение ссылки, чтобы страница не перезагружалась.
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  header.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
